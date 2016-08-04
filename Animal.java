33f8fc156bd49a2369fe230bf3dbddc111bb9fb9
clintonmu <clintonmu@gmail.com>
2016-08-04 12:28:12 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,32 @@
+
+public class Animal {
+String color;
+int age;
+int height;
+int weight;
+boolean ancestor;
+boolean extinct;
+public boolean doesItMakeSound()
+{
+	return true;
+}
+public boolean doesItEat()
+{
+	return true;
+}
+
+public boolean doesItDrink()
+{
+	return true;
+}
+public boolean doesItSpeak()
+{
+	return false;
+}
+
+public void whatSoundItMakes()
+{
+	System.out.println("Animal not have any specif sound");
+}
+	
+}
